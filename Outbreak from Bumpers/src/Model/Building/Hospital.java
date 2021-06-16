4d3e6570170e7199b9b47ee6c61b5f3f5469296e
Julian <j.kraus@tum.de>
2021-06-16 22:37:21 +0200

    upload

---
+++ Outbreak from Bumpers/src/Model/Building/Hospital.java
@@ -0,0 +1,9 @@
+package Model.Building;
+
+import Model.Building.Building;
+
+public class Hospital extends Building {
+    public Hospital(double[] initialPosition, double width, double height) {
+        super(initialPosition, width, height);
+    }
+}
